704ad4eb958a4a9813eaf3f7e28a44fd309a2577
Areimo <oreoro2023@gmail.com>
2025-01-29 19:44:53 -0300

    Add files via upload

---
+++ Calculadora_De_Promedio.py
@@ -0,0 +1,12 @@
+calificaciones = 1
+total = 0
+
+while calificaciones >8:
+    notas=input('Ingrese sus notas')
+    total+=int(notas)
+    calificaciones+=1
+promedio=total/12
+
+print('Su promedio total es:', promedio)
+
+#hmcp lpgz xjws vvca
